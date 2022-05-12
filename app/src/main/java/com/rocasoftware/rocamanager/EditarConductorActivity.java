09b5aa8fceb615fd6699b173e56c640440554e07
razuara <rodolfo.azuara@gmail.com>
2022-05-12 11:44:02 -0500

    EditarConductorActivity terminado

--- app/src/main/java/com/rocasoftware/rocamanager/EditarConductorActivity.java
+++ app/src/main/java/com/rocasoftware/rocamanager/EditarConductorActivity.java
@@ -5,8 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -15,24 +21,38 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 public class EditarConductorActivity extends AppCompatActivity {
 
-    TextView pruebaTextView,nombreTextView;
+    TextView nombreEditText,apellidoEditText,telefonoEditText,emailEditText,passwordEditText,repetirPasswordEditText;
+    Button actualizarButton;
     DatabaseReference mDatabase;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_conductor);
 
-        pruebaTextView = findViewById(R.id.pruebaTextView);
-        nombreTextView = findViewById(R.id.nombreTextView);
+        nombreEditText = findViewById(R.id.nombreEditText);
+        apellidoEditText = findViewById(R.id.apellidoEditText);
+        telefonoEditText = findViewById(R.id.telefonoEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        repetirPasswordEditText = findViewById(R.id.repetirPasswordEditText);
+        actualizarButton = findViewById(R.id.actualizarButton);
+
+
+
+        emailEditText.setFocusable(false);
+        emailEditText.setClickable(false);
 
         String idConductor = getIntent().getStringExtra("idConductor");
 
-        pruebaTextView.setText(idConductor);
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         mDatabase = FirebaseDatabase.getInstance().getReference("conductores").child(user.getUid());
 
         mDatabase.child(idConductor).addValueEventListener(new ValueEventListener() {
@@ -41,17 +61,91 @@ public class EditarConductorActivity extends AppCompatActivity {
                 if (snapshot.exists())
                 {
                     String nombre = snapshot.child("nombre").getValue().toString();
-                    nombreTextView.setText(nombre);
+                    String apellido = snapshot.child("apellido").getValue().toString();
+                    String telefono = snapshot.child("telefono").getValue().toString();
+                    String email = snapshot.child("email").getValue().toString();
+                    String password = snapshot.child("password").getValue().toString();
+                    String repetirPassword = snapshot.child("password").getValue().toString();
+                    nombreEditText.setText(nombre);
+                    apellidoEditText.setText(apellido);
+                    telefonoEditText.setText(telefono);
+                    emailEditText.setText(email);
+                    passwordEditText.setText(password);
+                    repetirPasswordEditText.setText(repetirPassword);
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                nombreTextView.setText("Error");
+                nombreEditText.setText("Error");
+            }
+        });
+
+        actualizarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate();
             }
         });
 
 
-
     }
+
+    private void validate() {
+
+        String nombre = nombreEditText.getText().toString().trim();
+        String apellido = apellidoEditText.getText().toString().trim();
+        String telefono = telefonoEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+        String repetirPassword = repetirPasswordEditText.getText().toString().trim();
+
+
+        if (password.isEmpty() || password.length() < 8)
+        {
+            passwordEditText.setError("Se necesitan mas de 8 caracteres");
+            return;
+        }
+        else if(!Pattern.compile("[0-9]").matcher(password).find())
+        {
+            passwordEditText.setError("Al menos un numero");
+            return;
+        }
+        else
+        {
+            passwordEditText.setError(null);
+        }
+        if (!repetirPassword.equals(password))
+        {
+            repetirPasswordEditText.setError("Deben ser iguales");
+            return;
+        }
+        else
+        {
+            actualizar(nombre,apellido,telefono,password);
+        }
+    }
+    private void actualizar(String nombre,String apellido,String telefono, String password) {
+
+        String idConductor = getIntent().getStringExtra("idConductor");
+        mDatabase = FirebaseDatabase.getInstance().getReference("conductores").child(user.getUid()).child(idConductor);
+
+        mDatabase.child("nombre").setValue(nombre);
+        mDatabase.child("apellido").setValue(apellido);
+        mDatabase.child("telefono").setValue(telefono);
+        mDatabase.child("password").setValue(password);
+        mDatabase.child("nombreCompleto").setValue(nombre + " " + apellido);
+
+        Intent intent = new Intent(EditarConductorActivity.this,ConductoresActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(EditarConductorActivity.this,ConductoresActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
